f451097f4a67e8c5a55287628974977e5b2dc421
patriciobengoechea <patriciobengoechea@10.0.22.245>
2017-07-28 17:45:26 -0300

    Creacion clase Elemento

--- src/entities/Elemento.java
+++ src/entities/Elemento.java
@@ -5,5 +5,29 @@ public class Elemento {
 	private String nombre;
 	private String descripcion;
 	private int cantidad;
+	public int getIdElemento() {
+		return idElemento;
+	}
+	public void setIdElemento(int idElemento) {
+		this.idElemento = idElemento;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 
 }
